edefd5c8a8024e411805d6b71333c7806e63662f
Samet <sametselki@outlook.com>
2022-05-24 07:06:49 +0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Componnets from "./components";
+
+function App() {
+  return (
+    <div className="App">
+      <Componnets />
+    </div>
+  );
+}
+
+export default App;
